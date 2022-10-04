45550a0564bd5759ba41c25ab048544773b5a88f
DenisPeleng <rangerby@icloud.com>
2022-10-04 17:22:51 +0400

    Completed Stage 4 Changed x/y orientation

--- Minesweeper/task/src/minesweeper/FieldMinesweeper.java
+++ Minesweeper/task/src/minesweeper/FieldMinesweeper.java
@@ -3,19 +3,23 @@ package minesweeper;
 import java.util.Random;
 
 public class FieldMinesweeper {
-    int fieldSize;
-    char[][] field;
-    int amountOfMines;
+    private final int fieldSizeX;
+    private final int fieldSizeY;
+    private final char[][] fieldInGame;
+    private final char[][] fieldWithAnswer;
+    private int amountOfMines;
 
-    FieldMinesweeper(int fieldSize) {
-        this.fieldSize = fieldSize;
-        this.field = createField(fieldSize);
+    FieldMinesweeper(int fieldSizeX, int fieldSizeY) {
+        this.fieldSizeX = fieldSizeX;
+        this.fieldSizeY = fieldSizeY;
+        this.fieldInGame = createField(fieldSizeX, fieldSizeY);
+        this.fieldWithAnswer = createField(fieldSizeX, fieldSizeY);
     }
 
-    public char[][] createField(int fieldSize) {
-        char[][] field = new char[fieldSize][fieldSize];
-        for (int i = 0; i < fieldSize; i++) {
-            for (int j = 0; j < fieldSize; j++) {
+    public char[][] createField(int fieldSizeX, int fieldSizeY) {
+        char[][] field = new char[fieldSizeY][fieldSizeX];
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
                 field[i][j] = '.';
             }
         }
@@ -27,32 +31,40 @@ public class FieldMinesweeper {
         int currentAmountOfMines = 0;
         this.amountOfMines = amountOfMines;
         while (currentAmountOfMines != amountOfMines) {
-            int randomX = random.nextInt(fieldSize);
-            int randomY = random.nextInt(fieldSize);
-            if (field[randomX][randomY] != 'X') {
-                field[randomX][randomY] = 'X';
+            int randomX = random.nextInt(fieldSizeX);
+            int randomY = random.nextInt(fieldSizeY);
+            if (fieldWithAnswer[randomY][randomX] != 'X') {
+                fieldWithAnswer[randomY][randomX] = 'X';
                 currentAmountOfMines++;
             }
         }
     }
 
     public void printField() {
-
-        for (int i = 0; i < fieldSize; i++) {
-            for (int j = 0; j < fieldSize; j++) {
-                System.out.print(field[i][j]);
+        System.out.print(" |");
+        for (int i = 0; i < fieldSizeY; i++) {
+            System.out.print(i + 1);
+        }
+        System.out.println("|");
+        System.out.println("-|---------|");
+        for (int i = 0; i < fieldSizeY; i++) {
+            System.out.printf("%d|", i + 1);
+            for (int j = 0; j < fieldSizeX; j++) {
+                System.out.print(fieldInGame[i][j]);
             }
+            System.out.print("|");
             System.out.println();
         }
+        System.out.println("-|---------|");
     }
 
     private int checkMinesAround(int x, int y) {
         int amountOfMines = 0;
         for (int i = x - 1; i < x + 2; i++) {
-            if (i >= 0 && i < fieldSize) {
+            if (i >= 0 && i < fieldSizeY) {
                 for (int j = y - 1; j < y + 2; j++) {
-                    if (j >= 0 && j < fieldSize) {
-                        if (field[i][j] == 'X') {
+                    if (j >= 0 && j < fieldSizeX) {
+                        if (fieldWithAnswer[i][j] == 'X') {
                             amountOfMines++;
                         }
                     }
@@ -63,15 +75,54 @@ public class FieldMinesweeper {
     }
 
     public void fillFieldWithAmountOfMines() {
-        for (int i = 0; i < fieldSize; i++) {
-            for (int j = 0; j < fieldSize; j++) {
-                if (field[i][j] != 'X') {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (fieldWithAnswer[i][j] != 'X') {
                     int amountOfMinesAround = checkMinesAround(i, j);
                     if (amountOfMinesAround > 0) {
-                        field[i][j] = (char) (amountOfMinesAround + '0');
+                        fieldInGame[i][j] = (char) (amountOfMinesAround + '0');
                     }
                 }
             }
         }
+    }
+
+    public void fillPlayerTurn(int x, int y) {
+        if (x <= 0 && x < fieldSizeX) {
+            System.out.println("Error x coordinate");
+            return;
+        }
+        if (y <= 0 && x < fieldSizeY) {
+            System.out.println("Error y coordinate");
+            return;
+        }
+        x = x - 1;
+        y = y - 1;
+        if (fieldInGame[y][x] == '.') {
+            fieldInGame[y][x] = '*';
+        } else if (fieldInGame[y][x] == '*') {
+            fieldInGame[y][x] = '.';
+
+        } else {
+            System.out.println("There is a number here!");
+        }
+
+    }
+
+
+    public boolean isUserWin() {
+        int amountOfCorrectCheckedMines = 0;
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (fieldInGame[i][j] == '*' && fieldWithAnswer[i][j] == 'X') {
+                    amountOfCorrectCheckedMines++;
+                }
+            }
+        }
+        if (amountOfMines == amountOfCorrectCheckedMines) {
+            System.out.println("Congratulations! You found all the mines!");
+            return true;
+        }
+        return false;
     }
 }
--- Minesweeper/task/src/minesweeper/Main.java
+++ Minesweeper/task/src/minesweeper/Main.java
@@ -3,15 +3,27 @@ package minesweeper;
 import java.util.Scanner;
 
 public class Main {
+    static boolean isGameRun = true;
+
     public static void main(String[] args) {
-        int fieldSize = 9;
+        int fieldSizeX = 9;
+        int fieldSizeY = 9;
         Scanner scanner = new Scanner(System.in);
-        FieldMinesweeper fieldMinesweeper = new FieldMinesweeper(fieldSize);
+        FieldMinesweeper fieldMinesweeper = new FieldMinesweeper(fieldSizeX, fieldSizeY);
         System.out.println("How many mines do you want on the field?");
         int amountOfMines = scanner.nextInt();
         fieldMinesweeper.fillMinesOnField(amountOfMines);
         fieldMinesweeper.fillFieldWithAmountOfMines();
         fieldMinesweeper.printField();
+        while (isGameRun) {
+            System.out.println("Set/delete mines marks (x and y coordinates): ");
+            int currentX = scanner.nextInt();
+            int currentY = scanner.nextInt();
+            fieldMinesweeper.fillPlayerTurn(currentX, currentY);
+            fieldMinesweeper.printField();
+            isGameRun = !fieldMinesweeper.isUserWin();
+
+        }
     }
 
 
