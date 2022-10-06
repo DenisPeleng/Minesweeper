0002ebbf8478be0b46f65ca43403fd08f770c138
DenisPeleng <rangerby@icloud.com>
2022-10-06 18:21:48 +0400

    Completed Project

--- Minesweeper/task/src/minesweeper/FieldMinesweeper.java
+++ Minesweeper/task/src/minesweeper/FieldMinesweeper.java
@@ -1,5 +1,6 @@
 package minesweeper;
 
+import java.util.ArrayDeque;
 import java.util.Random;
 
 public class FieldMinesweeper {
@@ -7,7 +8,10 @@ public class FieldMinesweeper {
     private final int fieldSizeY;
     private final char[][] fieldInGame;
     private final char[][] fieldWithAnswer;
-    private int amountOfMines;
+    int amountOfMines;
+    private boolean isSteppedOnMine = false;
+    private final ArrayDeque<Integer> arrayDequeColumnToCheck = new ArrayDeque<>();
+    private final ArrayDeque<Integer> arrayDequeRowToCheck = new ArrayDeque<>();
 
     FieldMinesweeper(int fieldSizeX, int fieldSizeY) {
         this.fieldSizeX = fieldSizeX;
@@ -26,18 +30,25 @@ public class FieldMinesweeper {
         return field;
     }
 
-    public void fillMinesOnField(int amountOfMines) {
-        Random random = new Random();
-        int currentAmountOfMines = 0;
+    public void setAmountOfMines(int amountOfMines) {
         this.amountOfMines = amountOfMines;
+    }
+
+    public void fillMinesOnField(int currentX, int currentY) {
+        Random random = new Random();
+
+        int currentAmountOfMines = 0;
         while (currentAmountOfMines != amountOfMines) {
             int randomX = random.nextInt(fieldSizeX);
             int randomY = random.nextInt(fieldSizeY);
-            if (fieldWithAnswer[randomY][randomX] != 'X') {
+            if (fieldWithAnswer[randomY][randomX] != 'X'
+                    && currentX != randomX
+                    || currentY != randomY) {
                 fieldWithAnswer[randomY][randomX] = 'X';
                 currentAmountOfMines++;
             }
         }
+        this.fillFieldWithAmountOfMinesAround();
     }
 
     public void printField() {
@@ -74,30 +85,28 @@ public class FieldMinesweeper {
         return amountOfMines;
     }
 
-    public void fillFieldWithAmountOfMines() {
+    public void fillFieldWithAmountOfMinesAround() {
         for (int i = 0; i < fieldSizeY; i++) {
             for (int j = 0; j < fieldSizeX; j++) {
                 if (fieldWithAnswer[i][j] != 'X') {
                     int amountOfMinesAround = checkMinesAround(i, j);
                     if (amountOfMinesAround > 0) {
-                        fieldInGame[i][j] = (char) (amountOfMinesAround + '0');
+                        fieldWithAnswer[i][j] = (char) (amountOfMinesAround + '0');
                     }
                 }
             }
         }
     }
 
-    public void fillPlayerTurn(int x, int y) {
-        if (x <= 0 && x < fieldSizeX) {
+    public void markCellAsMined(int x, int y) {
+        if (x < 0 && x > fieldSizeX) {
             System.out.println("Error x coordinate");
             return;
         }
-        if (y <= 0 && x < fieldSizeY) {
+        if (y < 0 && y > fieldSizeY) {
             System.out.println("Error y coordinate");
             return;
         }
-        x = x - 1;
-        y = y - 1;
         if (fieldInGame[y][x] == '.') {
             fieldInGame[y][x] = '*';
         } else if (fieldInGame[y][x] == '*') {
@@ -110,8 +119,13 @@ public class FieldMinesweeper {
     }
 
 
-    public boolean isUserWin() {
+    public boolean isGameRunning() {
+        if (isSteppedOnMine) {
+            System.out.println("You stepped on a mine and failed!");
+            return false;
+        }
         int amountOfCorrectCheckedMines = 0;
+        int amountOfUncheckedMines = 0;
         for (int i = 0; i < fieldSizeY; i++) {
             for (int j = 0; j < fieldSizeX; j++) {
                 if (fieldInGame[i][j] == '*' && fieldWithAnswer[i][j] == 'X') {
@@ -119,10 +133,108 @@ public class FieldMinesweeper {
                 }
             }
         }
-        if (amountOfMines == amountOfCorrectCheckedMines) {
-            System.out.println("Congratulations! You found all the mines!");
-            return true;
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (fieldInGame[i][j] == '/') {
+                    amountOfUncheckedMines++;
+                }
+            }
         }
-        return false;
+        if (amountOfMines == amountOfCorrectCheckedMines ||
+                amountOfMines == fieldSizeX * fieldSizeY - amountOfUncheckedMines) {
+            System.out.println("Congratulations! You found all the mines!");
+            return false;
+        }
+
+        return true;
+    }
+
+    public void claimCellAsFree(int x, int y) {
+        if (fieldWithAnswer[y][x] == 'X') {
+            isSteppedOnMine = true;
+            for (int i = 0; i < fieldSizeY; i++) {
+                for (int j = 0; j < fieldSizeX; j++) {
+                    if (fieldInGame[i][j] == '.' && fieldWithAnswer[i][j] == 'X') {
+                        fieldInGame[i][j] = 'X';
+                    }
+                }
+            }
+            return;
+        }
+
+        arrayDequeColumnToCheck.push(x);
+        arrayDequeRowToCheck.push(y);
+        while (!arrayDequeColumnToCheck.isEmpty() && !arrayDequeRowToCheck.isEmpty()) {
+            checkAndMarkEmptyCellsInRow(arrayDequeColumnToCheck.pop(), arrayDequeRowToCheck.pop());
+        }
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (fieldInGame[i][j] == '/') {
+                    markCellAroundEmpty(i, j);
+                }
+            }
+        }
+    }
+
+    private void checkAndMarkEmptyCellsInRow(int x, int y) {
+        for (int i = x; i < fieldSizeX; i++) {
+
+            if (fieldInGame[y][i] == '.' && fieldWithAnswer[y][i] == '.') {
+                fieldInGame[y][i] = '/';
+                checkUpDownCells(i, y);
+                fieldWithAnswer[y][i] = '/';
+            } else if (fieldWithAnswer[y][i] == '.' && fieldInGame[y][i] == '*') {
+                fieldInGame[y][i] = '/';
+                checkUpDownCells(i, y);
+                fieldWithAnswer[y][i] = '/';
+            } else if (fieldWithAnswer[y][i] != 'X'
+                    && fieldWithAnswer[y][i] != '.') {
+                fieldInGame[y][i] = fieldWithAnswer[y][i];
+                break;
+            }
+        }
+
+        for (int i = x - 1; i >= 0; i--) {
+            if (fieldInGame[y][i] == '.' && fieldWithAnswer[y][i] == '.') {
+                fieldInGame[y][i] = '/';
+                checkUpDownCells(i, y);
+                fieldWithAnswer[y][i] = '/';
+            } else if (fieldWithAnswer[y][i] == '.' && fieldInGame[y][i] == '*') {
+                fieldInGame[y][i] = '/';
+                checkUpDownCells(i, y);
+                fieldWithAnswer[y][i] = '/';
+            } else if (fieldWithAnswer[y][i] != 'X'
+                    && fieldWithAnswer[y][i] != '.') {
+                fieldInGame[y][i] = fieldWithAnswer[y][i];
+                break;
+            }
+        }
+
+    }
+
+    private void checkUpDownCells(int x, int y) {
+        if (y + 1 < fieldSizeY) {
+            arrayDequeColumnToCheck.push(x);
+            arrayDequeRowToCheck.push(y + 1);
+        }
+        if (y - 1 >= 0) {
+            arrayDequeColumnToCheck.push(x);
+            arrayDequeRowToCheck.push(y - 1);
+        }
+    }
+
+    private void markCellAroundEmpty(int x, int y) {
+        for (int i = x - 1; i < x + 2; i++) {
+            if (i >= 0 && i < fieldSizeY) {
+                for (int j = y - 1; j < y + 2; j++) {
+                    if (j >= 0 && j < fieldSizeX) {
+
+                        if (fieldWithAnswer[i][j] != 'X') {
+                            fieldInGame[i][j] = fieldWithAnswer[i][j];
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- Minesweeper/task/src/minesweeper/Main.java
+++ Minesweeper/task/src/minesweeper/Main.java
@@ -8,20 +8,48 @@ public class Main {
     public static void main(String[] args) {
         int fieldSizeX = 9;
         int fieldSizeY = 9;
+        boolean isCorrectAmountOfMines = false;
+        boolean isFirstTurn = true;
+        int amountOfMines;
         Scanner scanner = new Scanner(System.in);
+
         FieldMinesweeper fieldMinesweeper = new FieldMinesweeper(fieldSizeX, fieldSizeY);
-        System.out.println("How many mines do you want on the field?");
-        int amountOfMines = scanner.nextInt();
-        fieldMinesweeper.fillMinesOnField(amountOfMines);
-        fieldMinesweeper.fillFieldWithAmountOfMines();
+
+        while (!isCorrectAmountOfMines) {
+            System.out.println("How many mines do you want on the field?");
+            amountOfMines = scanner.nextInt();
+            fieldMinesweeper.setAmountOfMines(amountOfMines);
+            if (amountOfMines < fieldSizeX * fieldSizeY) {
+                isCorrectAmountOfMines = true;
+            } else {
+                System.out.println("Error, choose smaller amount of mines");
+            }
+        }
+
         fieldMinesweeper.printField();
         while (isGameRun) {
-            System.out.println("Set/delete mines marks (x and y coordinates): ");
-            int currentX = scanner.nextInt();
-            int currentY = scanner.nextInt();
-            fieldMinesweeper.fillPlayerTurn(currentX, currentY);
-            fieldMinesweeper.printField();
-            isGameRun = !fieldMinesweeper.isUserWin();
+            System.out.println("Set/unset mine marks or claim a cell as free:");
+            int currentX = scanner.nextInt() - 1;
+            int currentY = scanner.nextInt() - 1;
+            String action = scanner.next();
+            switch (action) {
+                case "free" -> {
+                    if (isFirstTurn) {
+                        fieldMinesweeper.fillMinesOnField(currentX, currentY);
+                        isFirstTurn = false;
+                    }
+                    fieldMinesweeper.claimCellAsFree(currentX, currentY);
+                    fieldMinesweeper.printField();
+                    isGameRun = fieldMinesweeper.isGameRunning();
+                }
+                case "mine" -> {
+                    fieldMinesweeper.markCellAsMined(currentX, currentY);
+                    fieldMinesweeper.printField();
+                    isGameRun = fieldMinesweeper.isGameRunning();
+                }
+                default -> System.out.println("Wrong command");
+            }
+
 
         }
     }
